44bcbb481bb8f6f84506b7101c2e50a6d89b42ff
mihai135 <mihai135@yahoo.com>
2019-03-25 12:34:29 +0200

    front-end

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -17,23 +17,82 @@ window.ToDoList = {
             method: "POST",
             contentType: "application/json; charset=utf-8",
             data: JSON.stringify(data)
-        }.done(function(response){
+        }).done(function(response){
             console.log(response);
             //reload items table
 
-        }));
+        });
+    },
+
+    getItemRow: function(item){
+
+        var checkedAttribute = item.done ? 'checked' :'';
+        var deadline = new Date(item.deadline).toLocaleDateString('ro-RO');
+        return `<tr>
+<td class="description">${item.description}</td>
+<td class="deadline">${deadline}</td>
+<td><input type="checkbox" ${checkedAttribute} class="mark-done" title="Done"></td>
+<td><a href="#" class="fa fa-trash delete" data-id="${item.id}"></a></td>
+</tr>`
+    },
+
+    displayItems: function(items){
+        console.log('Displaying items');
+        var rows = '';
+        items.forEach(item => rows += ToDoList.getItemRow(item) );
+
+        console.log('rows');
+
+        $('#to-do-items tbody').html(rows);
+
+    },
+
+    getItems:function (){
+        $.ajax({
+            url: ToDoList.apiUrl,
+            method: "GET"
+        }).done(function(response){
+            console.log(response);
+            //reload items table
+
+            ToDoList.displayItems(response)
+
+        });
+
+
+    },
+
+    deleteItem:function(id){
+        $.ajax({
+            url: ToDoList.apiUrl + '?id=' + id,
+            method: "DELETE"
+        }).done(function(response){
+            console.log(response);
+            //reload items table
+
+            ToDoList.getItems();
+
+        });
+
     },
 
     bindEvents: function () {
-        $("#to-do-items").submit(function (event) {
+        $("#create-item-form").submit(function (event) {
             event.preventDefault();
+            console.log('Submitting form');
             ToDoList.addItem();
 
             return false;
 
-        })
+        });
+
+        $('#to-do-items tbody').delegate('.delete','click',function(){
+            var id = $(this).data('id');
+            ToDoList.deleteItem(id);
+        });
 
     }
 };
 
+ToDoList.getItems();
 ToDoList.bindEvents();
